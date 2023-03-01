2a00a8281380acca795552c392c0e23c2e725bcb
geoffreym2016 <geoffreym2016@gmail.com>
2023-03-01 13:34:47 -0800

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* string_toupper - changes all lowercase letters to uppper
+* @s: string to check
+*
+* Return: pointer to string
+*/
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+			*(s + i) = *(s + i) - 32;
+	}
+
+	return (s);
+}
