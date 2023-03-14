750d016eaa2b3e3fe0dc843c69560ccf82b37f34
geoffreym2016 <geoffreym2016@gmail.com>
2023-03-14 00:22:24 -0700

    I will show you how great I am

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * strtow - splits a string into words
+ *
+ * @str: string
+ *
+ * Return: char**, array or strings
+*/
+int **strtow(char *str)
+{
+	int **array;
+
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+
+	return (array);
+}
