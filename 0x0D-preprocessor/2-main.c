bb36d6679b86c68a989678367ff50382bd6e4580
geoffreym2016 <geoffreym2016@gmail.com>
2023-03-20 13:17:17 -0700

    2. File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point to the program. prints the name of the file it was
+ * compiled from
+ *
+ *Return: void
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return
+}
