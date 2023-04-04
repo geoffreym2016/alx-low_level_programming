f67a34713bd1ed5d1c93a99f2126180e36e76d7f
geoffreym2016 <geoffreym2016@gmail.com>
2023-04-04 23:22:04 +0300

    lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,23 +1,21 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef lists_h
+#define lists_h
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <stddef.h>
 
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
- * Description: singly linked list node structure
- * 
+ * Description: Signly linked list node structure
+ *
  */
 typedef struct listint_s
 {
-int n;
-struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
-
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -30,5 +28,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
 
-#endif
+#endif /* lists_h */
